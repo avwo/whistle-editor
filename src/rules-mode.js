1330e58ee3462195ee5583e278cfce6ab079cd8b
avwo <avwu@qq.com>
2021-08-05 10:38:13 +0800

    fix: shift-1 error

--- src/rules-mode.js
+++ src/rules-mode.js
@@ -251,6 +251,9 @@ CodeMirror.defineMode('rules', () => {
         pre = ch;
         return true;
       });
+      if (!str) {
+        return;
+      }
       if (!type) {
         if (isRegExp(str) || isRegUrl(str) || isPortPattern(str)) {
           return 'attribute js-attribute';
